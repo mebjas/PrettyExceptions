f23eff289df0ffc4c2ad3ceb3e1b1602aee09e73
Minhaz Vadakekara <minhazv@microsoft.com>
2018-11-01 00:52:10 +0530

    Path corrected, test internally

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './App.css';
-import Window from './window/Window'
+import Window from './Window/Window'
 
 class App extends React.Component {
   public render() {
